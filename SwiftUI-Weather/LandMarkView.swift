f077e8b9564620c36afdf2a6e92cf7e32d6ecf79
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-12-05 20:38:26 +0530

    Text views added

---
+++ SwiftUI-Weather/LandMarkView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct LandMarkView: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct LandMarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandMarkView()
+    }
+}

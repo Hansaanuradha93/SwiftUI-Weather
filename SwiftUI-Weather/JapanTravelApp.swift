13de84467c53ed182c20ef3a212b4f13b3c4e413
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-12-21 20:18:33 +0530

    Header designed

--- SwiftUI-Weather/JapanTravelApp.swift
+++ SwiftUI-Weather/JapanTravelApp.swift
@@ -2,12 +2,27 @@ import SwiftUI
 
 struct JapanTravelApp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Header()
+        }
     }
 }
 
 struct JapanTravelApp_Previews: PreviewProvider {
     static var previews: some View {
         JapanTravelApp()
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        HStack {
+            Text("Discover")
+                .font(.system(size: 44, weight: .bold, design: .rounded))
+            Spacer()
+            Image(systemName: "magnifyingglass")
+                .font(.system(size: 28, weight: .light))
+        }
+        .padding(.horizontal, 20)
     }
 }

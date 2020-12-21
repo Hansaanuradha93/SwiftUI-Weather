9cdef1cefbd73b5674a1769fe9dfb815f9acd3fc
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-12-21 20:08:46 +0530

    Japan Travel App created

---
+++ SwiftUI-Weather/JapanTravelApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct JapanTravelApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JapanTravelApp_Previews: PreviewProvider {
+    static var previews: some View {
+        JapanTravelApp()
+    }
+}
